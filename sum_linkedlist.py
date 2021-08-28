772b8f7a5af9e43bb267b1121043d2a7398d92f0
MaximLKorenev <mlkorenev@yandex.ru>
2021-08-28 11:18:31 +0300

    new

---
+++ sum_linkedlist.py
@@ -0,0 +1,29 @@
+import LinkedList
+
+a = LinkedList.LinkedList()
+b = LinkedList.LinkedList()
+
+a.add_in_tail(LinkedList.Node(1))
+a.add_in_tail(LinkedList.Node(2))
+a.add_in_tail(LinkedList.Node(3))
+a.add_in_tail(LinkedList.Node(4))
+a.add_in_tail(LinkedList.Node(5))
+
+b.add_in_tail(LinkedList.Node(5))
+b.add_in_tail(LinkedList.Node(4))
+b.add_in_tail(LinkedList.Node(3))
+b.add_in_tail(LinkedList.Node(2))
+b.add_in_tail(LinkedList.Node(1))
+
+
+def sum_link_list(first, second):
+    if first.len() == second.len():
+        c = LinkedList.LinkedList()
+        n = first.len()
+        a = first.head
+        b = second.head
+        for i in range(n):
+            c.add_in_tail(LinkedList.Node(a.value + b.value))
+            a = a.next
+            b = b.next
+        return c
