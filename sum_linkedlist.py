017202c3d51fb109e321aedbf30688b094816709
MaximLKorenev <73540257+MaximLKorenev@users.noreply.github.com>
2021-08-28 18:51:00 +0300

    Update sum_linkedlist.py

--- sum_linkedlist.py
+++ sum_linkedlist.py
@@ -17,14 +17,14 @@ b.add_in_tail(LinkedList.Node(1))
 
 
 def sum_link_list(first, second):
-    if first.len() == second.len():
-        c = LinkedList.LinkedList()
-        n = first.len()
-        a = first.head
-        b = second.head
-        for i in range(n):
-            c.add_in_tail(LinkedList.Node(a.value + b.value))
-            a = a.next
-            b = b.next
-        return c
-    return None
+    if first.len() != second.len():
+        return None
+    c = LinkedList.LinkedList()
+    n = first.len()
+    a = first.head
+    b = second.head
+    for i in range(n):
+        c.add_in_tail(LinkedList.Node(a.value + b.value))
+        a = a.next
+        b = b.next
+    return c
